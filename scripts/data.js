f2d8a5bfa857747dd95362fb67f6766ccf8cc437
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2019-07-26 15:55:47 -0500

    Fully DOMed - Next goal is to add/replace new list of parks

--- scripts/data.js
+++ scripts/data.js
@@ -4,7 +4,7 @@ const parkSearch = document.querySelector("#parkSearchField")
 
 
 const parkFetcher = () => {
-    return fetch(`https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=uyvbFrUZ9I6eWTToRXt5hNAvw&${parkSearch.value}=Yes`)
+    return fetch(`https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=uyvbFrUZ9I6eWTToRXt5hNAvw&${parkSearch.value}=Yes&$limit=10`)
         .then(data => data.json())
 }
 

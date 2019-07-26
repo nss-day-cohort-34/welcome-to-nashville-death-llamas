e2e79a0b00248cdebedd1d159c7b03061b06d5b6
Ellie Ash <ellieash4000@gmail.com>
2019-07-26 13:11:17 -0500

    basic styling

--- scripts/data.js
+++ scripts/data.js
@@ -3,9 +3,5 @@
 getConcertData = () => {
     return fetch(" https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=QPEjElLoY6LMzzk0gwmG1gZcaWoXb93y")
     .then(concerts => concerts.json())
-    .then(concerts => {
-        for (const concert of concerts._embedded.events) {
-            console.log(concert.name)
-        }
-    })
+    
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,17 @@
 // This is where we call functions and pull our hair out
 
+const getConcertFetch = () => {
+getConcertData().then(concerts => {
+    for (const concert of concerts._embedded.events) {
+        console.log(concert)
+    }
+})
+}
 
 const getConcertsButton = document.getElementById("concertsSearchButton")
 
-getConcertsButton.addEventListener("click", () =>{
-    getConcertData();
+getConcertsButton.addEventListener("click", () => {
+    getConcertFetch();
+    console.log("im clicked")
 })
+

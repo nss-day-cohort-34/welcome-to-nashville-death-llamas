01ccbe4fdb0b6609554960aa4e1c82090be75458
Ellie Ash <ellieash4000@gmail.com>
2019-07-26 11:01:16 -0500

    button calls fetch

--- scripts/data.js
+++ scripts/data.js
@@ -1,1 +1,11 @@
 // This will contain all of our fetch calls from each of the four APIs
+
+getConcertData = () => {
+    return fetch(" https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=343&apikey=QPEjElLoY6LMzzk0gwmG1gZcaWoXb93y")
+    .then(concerts => concerts.json())
+    .then(concerts => {
+        for (const concert of concerts._embedded.events) {
+            console.log(concert.name)
+        }
+    })
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 // This is where we call functions and pull our hair out
+
+
+const getConcertsButton = document.getElementById("concertsSearchButton")
+
+getConcertsButton.addEventListener("click", () =>{
+    getConcertData();
+})

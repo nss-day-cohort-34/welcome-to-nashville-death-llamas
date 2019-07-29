32c419c6c13de4e213ea36b92647402f08058749
Noah Bartfield <noahbartfield@Noahs-MacBook-Pro.local>
2019-07-29 15:04:56 -0500

    Cleaned it up a bit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
 // This is where we call functions and pull our hair out
 
 
-// let parkSearch = document.querySelector("#parksSearchField").value
 const parkSearchButton = document.querySelector("#parkSearchButton")
 
 const fetchTheFetcher = () => {
     parkFetcher().then(parks => {
-        // console.log('parks: ', parks);
 
         for (let i = 0; i < parks.length; i++) {
             const park = parks[i];
@@ -60,25 +58,4 @@ const getConcertsButton = document.getElementById("concertsSearchButton")
 getConcertsButton.addEventListener("click", () => {
     getConcertFetch();
 })
-
-
-
-// let i = 0
-// const incrementID = () => {
-//     i++
-//     return i
-// }
-
-
-// const incrementedID = incrementID()
-
-// const cardFactory = () => {
-// `
-//     <section class="card--${incrementedID}">
-//         <p>${textarea.value}</p>
-//         <button class="deleteButton delete--${incrementedID}">Delete</button>
-//     </section>
-// `
-// }
-
 

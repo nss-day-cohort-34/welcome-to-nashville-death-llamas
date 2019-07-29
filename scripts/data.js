d7e40b90d48f8b6adbdc2c4f2ec478c0c2a263da
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-07-29 10:20:46 -0500

    added my code to primary files. deleted it from 'mr-*'

--- scripts/data.js
+++ scripts/data.js
@@ -11,3 +11,22 @@ const restarauntFetcher = () => {
 }
 
 restarauntFetcher()
+
+//matt's code below (does beg refactoring at "modularization phase")
+const searchMeetups = document.querySelector("#meetupsSearchField").value
+
+const getMeetups = () => {
+    return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchMeetups}&location.address=nashville&token=NAZKLO5PCLPRIX3ALUP5`)
+        .then(a => a.json())
+        .then(results => {
+            // console.log(results)
+            for (result of results.events) {
+                const htmlRep = meetupsFactory(result)
+                printToDOM(htmlRep)
+            }
+
+        })
+}
+
+const meetupsButton = document.querySelector("#meetupsSearch")
+meetupsButton.addEventListener("click", getMeetups)
--- scripts/dom.js
+++ scripts/dom.js
@@ -3,3 +3,13 @@ console.log(restarauntResults)
 const printToDOM = (htmlString) => {
     restarauntResults.innerHTML += htmlString
 }
+
+//matt's code below
+meetupsContainer = document.querySelector("#meetupResults")
+
+const printToDOM = (htmlString) => {
+    //take html string
+    meetupsContainer.innerHTML += htmlString
+    //add it to innerHTML of DOM location
+}
+
